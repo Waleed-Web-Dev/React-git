a638cb369123263b0805e35011a51f9d6f0e2819
Waleed Ahmed <sawasta556@gmail.com>
2024-08-08 22:45:19 +0500

    Appwrite Databse

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect  } from 'react'
 import './App.css'
+import {useDispatch} from "react-redux"
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import {Header, Footer } from './components'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    
-    <>
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(userData => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+ 
+   return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+<div className='w-full block'>
+  <Header />
+  <main>
+  TODO:  {/* <Outlet /> */}
+  </main>
+  <Footer />
+
+</div>
+    </div>
+   ) : (null)
   
-     <h3>Hellllllooooooo</h3>
-    </>
-  )
 }
 
 export default App
